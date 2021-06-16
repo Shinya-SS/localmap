e4f31c625f223a29ce4b1368e1e7ef0ed8049bce
Shinya-SS <cgyceusc@icloud.com>
2021-06-16 22:30:45 +0900

    #16 レイアウト修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,4 +1,5 @@
 class MypagesController < ApplicationController
+
   def index
     id = current_user.id
     query = "select TOPICS.* from topics inner join FAVORITES on TOPICS.id = FAVORITES.topic_id
@@ -9,7 +10,7 @@ class MypagesController < ApplicationController
     query_ranking = "select TOPICS.*,ranking.* from topics
     inner join (select topic_id ,count(1) as rank from favorites group by topic_id) as ranking
     on TOPICS.id = ranking.topic_id
-    order by ranking.rank desc,TOPICS.updated_at desc LIMIT 5"
+    order by ranking.rank desc,TOPICS.updated_at desc LIMIT 3"
     @topics_rank = Topic.find_by_sql([query_ranking])
     gon.topics_rank = @topics_rank
   end
