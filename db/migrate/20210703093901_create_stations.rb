e1dcf96fd0d310d919e90937bf04638991d3ae0b
Shinya-SS <cgyceusc@icloud.com>
2021-07-03 18:41:10 +0900

    posgre対応

--- db/migrate/20210703093901_create_stations.rb
+++ db/migrate/20210703093901_create_stations.rb
@@ -1,12 +1,12 @@
 class CreateStations < ActiveRecord::Migration[5.2]
   def change
-    create_table :stations do |t|
-      t.integer :station_id
+    create_table :stations, id: false do |t|
+      t.integer :station_id, null: false, unique: true
       t.integer :line_id
       t.string :name
       t.string :address
-      t.decimal :lat
-      t.decimal :lng
+      t.decimal :lat, :precision => 9, :scale => 6, null: true
+      t.decimal :lng, :precision => 9, :scale => 6, null: true
 
       t.datetime :created_at
       t.datetime :updated_at
