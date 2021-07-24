a0c65ba755020e85f24c071c8411cef78e176726
Shinya-SS <cgyceusc@icloud.com>
2021-07-24 23:39:33 +0900

    AWS対応

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,15 +3,15 @@ class MypagesController < ApplicationController
   def index
     # 自分がいいねした投稿上位３件
     id = current_user.id
-    query = "select TOPICS.* from topics inner join FAVORITES on TOPICS.id = FAVORITES.topic_id
-      where FAVORITES.user_id = #{id} order by updated_at desc LIMIT 3"
+    query = "select topics.* from topics inner join favorites on topics.id = favorites.topic_id
+    where favorites.user_id = #{id} order by topics.updated_at desc LIMIT 3"
     @topics = Topic.find_by_sql([query])
 
     # いいね数上位5件を取得
-    query_ranking = "select TOPICS.*,ranking.* from topics
+    query_ranking = "select topics.*,ranking.* from topics
     inner join (select topic_id ,count(1) as rank from favorites group by topic_id) as ranking
-    on TOPICS.id = ranking.topic_id
-    order by ranking.rank desc,TOPICS.updated_at desc LIMIT 5"
+    on topics.id = ranking.topic_id
+    order by ranking.rank desc, topics.updated_at desc LIMIT 5"
     @topics_rank = Topic.find_by_sql([query_ranking])
     gon.topics_rank = @topics_rank
 
